faa022c06c316837dce46b0a604aebb98c0c6c16
Luan <l.u.a.n@hotmail.com>
2026-02-26 20:49:43 -0300

    config

---
+++ src/services/jsCookie/index.ts
@@ -0,0 +1,13 @@
+import Cookies from 'js-cookie';
+
+export function setCookieAdapter(name: string, value: string) {
+  Cookies.set(name, value);
+}
+
+export function removeCookieAdapter(name: string) {
+  Cookies.remove(name);
+}
+
+export function getCookieAdapter(name: string) {
+  return Cookies.get(name);
+}
